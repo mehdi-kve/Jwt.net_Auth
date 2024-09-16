f9bab0a3d3529f110c9eff5be61f849f78e129c9
m3hdi <mehdi.k1917@gmail.com>
2024-09-16 14:03:14 +0330

    Initial project

---
+++ testAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using testAPI.Models;
+
+namespace testAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
---
+++ testAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace testAPI.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public DateTime ProduceDate { get; set; }
+        [StringLength(50)]
+        public string ManufacturePhone { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string ManufactureEmail { get; set; } = string.Empty;
+        public bool? IsAvailable { get; set; }
+    }
+}
